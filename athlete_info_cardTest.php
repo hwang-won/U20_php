480339b1ab5bde6c15ab040a419754d839bed2c6
NGN-Limjihoon <lonelynight1026@gmail.com>
2022-12-29 17:57:11 +0900

    feat: 참가자 이력서 카드 UI 구현

---
+++ athlete_info_cardTest.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/cardTestStyle.css" />
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css" />
+    <script src="/fontawesome/js/all.min.js"></script>
+    <title>U20</title>
+</head>
+
+<body cz-shortcut-listen="true">
+    <!-- contents 본문 내용 -->
+    <div class="container">
+        <div class="contents something">
+            <div class="contents club record">
+      <div class="f-wrap player" style="margin-top: 2.8571428571rem;">
+        <h3 class="tit-box style2">선수 정보</h3>
+        <div class="cont-box f-wrap left player-rank record-player detail">
+          <div class="img-box">
+            <img src="https://kleague-admin-test.s3.ap-northeast-2.amazonaws.com/v1/player/player_20220027.png" alt="">
+          </div>
+          <div class="txt-box">
+            <img src="/assets/images/emblem/K17_60.png" srcset="/assets/images/emblem/K17_60.png 1x, /assets/images/emblem/K17_60@2x.png 2x, /assets/images/emblem/K17_60@3x.png 3x" onerror="this.onerror=null; this.src='/assets/images/kemblem_60.png'; this.srcset='/assets/images/kemblem_60.png 1x ,/assets/images/kemblem_60@2x.png 2x ,/assets/images/kemblem_60@3x.png 3x';" alt="">
+            <div class="txt">
+              
+              <span class="name">가마
+                
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="cont-box right player-rank">
+          <table class="style2 center">
+            <tbody>
+              <tr>
+                <th>이름</th>
+                <td>가마</td>
+                <th>영문명</th>
+                <td>TORREIRA DA GAMA LIMA ALEXANDRE</td>
+              </tr>
+              <tr>
+                <th>소속구단</th>
+                <td>대구</td>
+                <th>포지션</th>
+                
+                  <td>-</td>
+                
+                
+              </tr>
+              <tr>
+                <th>배번</th>
+                
+                  <td>-</td>
+                
+                
+                <th>국적</th>
+                <td>브라질</td>
+              </tr>
+              <tr>
+                <th>키</th>
+                
+                  <td>-</td>
+                
+                
+                <th>몸무게</th>
+                
+                  <td>-</td>
+                
+                
+              </tr>
+              <tr>
+                <th>생년월일</th>
+                <td>1968/01/04</td>
+                <th></th>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      
+      <div class="f-wrap player-rank">
+        <h3 class="tit-box style2">감독기록</h3>
+        <div class="table-wrap">
+          <table>
+            <colgroup>
+              <col style="width: auto;">
+            </colgroup>
+            <thead>
+            <tr>
+              <th>리그</th>
+              <th>구단</th>
+              <th>재임년도</th>
+              <th>승</th>
+              <th>무</th>
+              <th>패</th>
+              <th>비고</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+              
+            
+              
+              <tr>
+                <td>K리그1</td>
+                <td>대구</td>
+                <td>2022</td>
+                <td>5</td>
+                <td>12</td>
+                <td>10</td>
+                <td>~2022/08/14</td>
+              </tr>
+              
+            
+
+            
+              
+                <tr>
+                  <td colspan="3">통산</td>
+                  <td>5</td>
+                  <td>12</td>
+                  <td>10</td>
+                  <td></td>
+                </tr>
+              
+            
+              
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+      
+
+      <div class="f-wrap player-rank">
+        <h3 class="tit-box style2">시즌기록</h3>
+        <div class="table-wrap">
+          <table>
+            <colgroup>
+              <col style="width: auto;">
+            </colgroup>
+            <thead>
+              <tr>
+                <th>연도</th>
+                <th>리그</th>
+                <th>소속</th>
+                <th>출장</th>
+                <th>득점</th>
+                <th>도움</th>
+                <th>골킥</th>
+                <th>코너킥</th>
+                <th>오프사이드</th>
+                <th>슈팅</th>
+                <th>파울</th>
+                <th>실점</th>
+                <th>경고</th>
+                <th>퇴장</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="f-wrap player-rank">
+        <h3 class="tit-box style2">통산</h3>
+        <div class="table-wrap">
+          <table>
+            <colgroup>
+              <col style="width: auto;">
+            </colgroup>
+            <thead>
+            <tr>
+              <th>대회</th>
+              <th>소속</th>
+              <th>출장</th>
+              <th>득점</th>
+              <th>도움</th>
+              <th>골킥</th>
+              <th>코너킥</th>
+              <th>오프사이드</th>
+              <th>슈팅</th>
+              <th>파울</th>
+              <th>실점</th>
+              <th>경고</th>
+              <th>퇴장</th>
+            </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="btn-wrap center">
+        <button class="btn btn-primary btn-sm" onclick="history.back();">돌아가기</button>
+      </div>
+    </div>
+</body>
+
+</html>
